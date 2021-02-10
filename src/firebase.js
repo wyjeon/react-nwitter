c02226c6e2cfe0811112b2304d4e41e3c39d4aaa
wyjeon <wyjeon.info@gmail.com>
2021-02-10 14:45:02 +0900

    #1.0 React + Firebase Setup 2

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase/app';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyA8nW5hwBNbsXF0xwYTey51dvb_bBhPq_A',
+  authDomain: 'nwitter-fd3ec.firebaseapp.com',
+  projectId: 'nwitter-fd3ec',
+  storageBucket: 'nwitter-fd3ec.appspot.com',
+  messagingSenderId: '33108135847',
+  appId: '1:33108135847:web:794a9638d37195f9a0f082',
+};
+
+// Initialize Firebase
+export default firebase.initializeApp(firebaseConfig);

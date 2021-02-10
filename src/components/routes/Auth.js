b42c0c64ca4510b585f7c32a5ea348297cc4d22e
wyjeon <wyjeon.info@gmail.com>
2021-02-10 18:26:46 +0900

    #2.3 Creating Account

--- src/components/routes/Auth.js
+++ src/components/routes/Auth.js
@@ -1,8 +1,10 @@
+import { authService } from 'fBase';
 import React, { useState } from 'react';
 
 const Auth = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [newAccount, setNewAccount] = useState(true);
 
   const onChange = event => {
     const {
@@ -15,16 +17,29 @@ const Auth = () => {
     }
   };
 
-  const onSubmit = event => {
+  const onSubmit = async event => {
     event.preventDefault();
+    try {
+      let data;
+      if (newAccount) {
+        //create account
+        data = await authService.createUserWithEmailAndPassword(email, password);
+      } else {
+        //log in
+        data = await authService.signInWithEmailAndPassword(email, password);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={email} name="email" type="text" placeholder="Email" required />
+        <input onChange={onChange} value={email} name="email" type="email" placeholder="Email" required />
         <input onChange={onChange} value={password} name="password" type="password" placeholder="Password" required />
-        <input type="submit" value="Log In" />
+        <input type="submit" value={newAccount ? 'Create Account' : 'Log In'} />
       </form>
       <div>
         <button>Continue with Google</button>

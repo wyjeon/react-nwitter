cd37458a422156ff36b8b1c2e9c4e81aacf68967
wyjeon <wyjeon.info@gmail.com>
2021-02-11 14:51:48 +0900

    #4.0 Preview Images part One

--- src/components/App.js
+++ src/components/App.js
@@ -19,12 +19,7 @@ function App() {
     });
   }, []);
 
-  return (
-    <>
-      {init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Initializing...'}
-      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
-    </>
-  );
+  return <>{init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Initializing...'}</>;
 }
 
 export default App;
--- src/components/routes/Home.js
+++ src/components/routes/Home.js
@@ -32,10 +32,21 @@ const Home = ({ userObj }) => {
     setNweet(value);
   };
 
+  const onFileChange = event => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = finishedEvent => {};
+    reader.readAsDataURL(theFile);
+  };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <input value={nweet} onChange={onchange} type="text" placeholder="what's on youre mind?" maxLength={120} />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
       </form>
       <div>

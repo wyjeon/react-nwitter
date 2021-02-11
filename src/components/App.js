7b9ee116ca3c131428eb1d2665ac4e243513bc91
wyjeon <wyjeon.info@gmail.com>
2021-02-11 17:31:08 +0900

    #5.2 Update Profile Bugfix

--- src/components/App.js
+++ src/components/App.js
@@ -11,7 +11,11 @@ function App() {
     authService.onAuthStateChanged(user => {
       if (user) {
         setIsLoggedIn(true);
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: args => user.updateProfile(args),
+        });
       } else {
         setIsLoggedIn(false);
       }
@@ -19,7 +23,16 @@ function App() {
     });
   }, []);
 
-  return <>{init ? <AppRouter isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Initializing...'}</>;
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: args => user.updateProfile(args),
+    });
+  };
+
+  return <>{init ? <AppRouter refreshUser={refreshUser} isLoggedIn={isLoggedIn} userObj={userObj} /> : 'Initializing...'}</>;
 }
 
 export default App;
--- src/components/routes/Profile.js
+++ src/components/routes/Profile.js
@@ -2,7 +2,7 @@ import { authService, dbService } from 'fBase';
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-const Profile = ({ userObj }) => {
+const Profile = ({ refreshUser, userObj }) => {
   const history = useHistory();
   const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
   const onLogOutClick = () => {
@@ -23,6 +23,7 @@ const Profile = ({ userObj }) => {
       await userObj.updateProfile({
         displayName: newDisplayName,
       });
+      refreshUser();
     }
   };
 
